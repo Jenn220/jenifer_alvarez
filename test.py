f20078967182c04c2ec6a815aa7ddb3dfe475816
Jenn220 <jennialvarez114@gmail.com>
2025-10-24 16:42:24 -0500

    Commit con error

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class TestApp(unittest.TestCase):
     def test_suma(self):
         response = self.app.get('/suma/5/3')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'5', response.data)
+        self.assertIn(b'0', response.data)
     
     def test_resta(self):
         response = self.app.get('/resta/10/4')
